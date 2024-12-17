468ce577765cf2e873ff531c8e4c56867c61356a
EL MAHDI HAKMOUNE <el-mahdi.hakmoune@fulll.fr>
2024-12-17 22:36:37 +0100

    Exploring the Docs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import "./App.css";
 import { Typography } from "@mui/material";
+import TourCard from "./components/TourCard";
 
 const App: React.FC = () => {
-  return <Typography variant="h3">Test MUI</Typography>;
+  return (
+    <>
+      <Typography variant="h3">Test MUI</Typography>
+      <TourCard />
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/TourCard.tsx
@@ -0,0 +1,11 @@
+import Paper from "@mui/material/Paper";
+
+const TourCard: React.FC = () => {
+  return (
+    <div>
+      <Paper elevation={3}>TourCard</Paper>
+    </div>
+  );
+};
+
+export default TourCard;
