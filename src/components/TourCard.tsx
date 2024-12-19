ef77b009d8b7de212d55d4e024222480f9014183
EL MAHDI HAKMOUNE <el-mahdi.hakmoune@fulll.fr>
2024-12-19 16:29:08 +0100

    Custom theme and provider

--- src/components/TourCard.tsx
+++ src/components/TourCard.tsx
@@ -2,64 +2,100 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid2";
 import { Box, Rating, Typography } from "@mui/material";
 import { AccessTime } from "@mui/icons-material";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+const theme = createTheme({
+  components: {
+    MuiTypography: {
+      variants: [
+        {
+          props: { variant: "body2" },
+          style: {
+            fontSize: 11,
+          },
+        },
+        {
+          props: { variant: "body3" },
+          style: {
+            fontSize: 10,
+          },
+        },
+      ],
+    },
+  },
+});
 
 const TourCard: React.FC = () => {
   return (
     <Grid size={3}>
-      <Paper elevation={3}>
-        <img
-          src="https://www.pelago.com/img/products/ID-Indonesia/bali-iconic-instagram-tour/1115-0741_bali-iconic-instagram-tour-indonesia-pelago0.jpg"
-          alt="bali"
-          className="img-card"
-        />
-        <Box sx={{ paddingX: 1 }}>
-          <Box>
-            <Typography variant="subtitle1" component="h2">
-              Immerse into the falls
-            </Typography>
-          </Box>
+      <ThemeProvider theme={theme}>
+        <Paper elevation={3}>
+          <img
+            src="https://www.pelago.com/img/products/ID-Indonesia/bali-iconic-instagram-tour/1115-0741_bali-iconic-instagram-tour-indonesia-pelago0.jpg"
+            alt="bali"
+            className="img-card"
+          />
+          <Box sx={{ paddingX: 1 }}>
+            <Box>
+              <Typography variant="subtitle1" component="h2">
+                Immerse into the falls
+              </Typography>
+            </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            <AccessTime sx={{ fontSize: "small" }} />
-            <Typography variant="body2" component="p" sx={{ marginLeft: 0.5 }}>
-              5 hours
-            </Typography>
-          </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <AccessTime sx={{ fontSize: "small" }} />
+              <Typography
+                variant="body2"
+                component="p"
+                sx={{ marginLeft: 0.5 }}
+              >
+                5 hours
+              </Typography>
+            </Box>
 
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              marginTop: 3,
-            }}
-          >
-            <Rating
-              name="read-only"
-              value={2.5}
-              precision={0.5}
-              size="small"
-              readOnly
-            />
-            <Typography variant="body2" component="p" sx={{ marginLeft: 0.5 }}>
-              2.5
-            </Typography>
-            <Typography variant="body2" component="p" sx={{ marginLeft: 1.5 }}>
-              (465 reviews)
-            </Typography>
-          </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                marginTop: 3,
+              }}
+            >
+              <Rating
+                name="read-only"
+                value={2.5}
+                precision={0.5}
+                size="small"
+                readOnly
+              />
+              <Typography
+                variant="body2"
+                component="p"
+                sx={{ marginLeft: 0.5 }}
+              >
+                2.5
+              </Typography>
+              <Typography
+                variant="body3"
+                component="p"
+                sx={{ marginLeft: 1.5 }}
+              >
+                (465 reviews)
+              </Typography>
+            </Box>
 
-          <Box>
-            <Typography variant="h6" component="h3">
-              From C $280
-            </Typography>
+            <Box>
+              <Typography variant="h6" component="h3">
+                From C $280
+              </Typography>
+            </Box>
           </Box>
-        </Box>
-      </Paper>
+        </Paper>
+      </ThemeProvider>
     </Grid>
   );
 };
